6f7d42f6fdc3f43b7171d6646f76a7ae52fe2b42
Owen Nelson <onelson@gmail.com>
2022-01-23 01:27:17 -0800

    refactor: simplify parser
    Since both fixed and displaced are optional (originally I thought we'd
    need one or the other or both), the parser could be simplified to one
    base pattern where both sides of the colon are optional.

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,8 @@
-use nom::bytes::complete::take;
-use nom::character::complete::alpha1;
-use nom::combinator::map_parser;
-use nom::sequence::{preceded, terminated};
 use nom::{
-    branch::alt,
+    bytes::complete::take,
+    character::complete::alpha1,
     character::complete::char as char_,
+    combinator::{map_parser, opt},
     multi::{count, many1},
     sequence::separated_pair,
     IResult,
@@ -22,52 +20,19 @@ fn char_parser(i: &str) -> IResult<&str, char> {
     Ok((i, c.chars().next().expect("char").to_ascii_lowercase()))
 }
 
-fn fixed_and_displaced(i: &str) -> IResult<&str, Column> {
-    let (i, (c, cs)) = separated_pair(count(char_parser, 1), char_(':'), many1(char_parser))(i)?;
-    Ok((
-        i,
-        Column {
-            fixed: Some(c[0]),
-            displaced: Some(cs),
-        },
-    ))
-}
-
-fn fixed_only(i: &str) -> IResult<&str, Column> {
-    let (i, c) = terminated(count(char_parser, 1), char_(':'))(i)?;
-    Ok((
-        i,
-        Column {
-            fixed: Some(c[0]),
-            displaced: None,
-        },
-    ))
-}
-
-fn empty(i: &str) -> IResult<&str, Column> {
-    let (i, _) = char_(':')(i)?;
-    Ok((
-        i,
-        Column {
-            fixed: None,
-            displaced: None,
-        },
-    ))
-}
-
-fn displaced_only(i: &str) -> IResult<&str, Column> {
-    let (i, cs) = preceded(char_(':'), many1(char_parser))(i)?;
-    Ok((
-        i,
-        Column {
-            fixed: None,
-            displaced: Some(cs),
-        },
-    ))
-}
-
 fn column_parser(i: &str) -> IResult<&str, Column> {
-    alt((fixed_and_displaced, fixed_only, displaced_only, empty))(i)
+    let (i, (c, cs)) = separated_pair(
+        opt(count(char_parser, 1)),
+        char_(':'),
+        opt(many1(char_parser)),
+    )(i)?;
+    Ok((
+        i,
+        Column {
+            fixed: c.map(|x| x[0]),
+            displaced: cs,
+        },
+    ))
 }
 
 impl FromStr for Column {
